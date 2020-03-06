29b20cf878425081aa52d2486699b43d90729414
Vladislav Burkatsky <feedmemorekurama@gmail.com>
2020-03-06 18:56:34 +0200

    Homework 4 completed

---
+++ homework_bernadska/hw_4/CheckPassFail.py
@@ -0,0 +1,8 @@
+print("Hello, welcome you in 'CheckPassFail' ")
+mark = int(input())
+print("Pass" if mark >= 50 else "Fail")
+print("Done")
+
+    
+
+
---
+++ homework_bernadska/hw_4/PrintDayInWord.py
@@ -0,0 +1,18 @@
+print("Print day in word!")
+day = int(input("Please enter num:"))
+if day == 1:
+    print("Today is Sunday")
+elif day == 2:
+    print("Today is Monday")
+elif day == 3:
+    print("Today is Tuesday")
+elif day == 4:
+    print("Today is Wednesday")
+elif day == 5:
+    print("Today is Thursday")
+elif day == 6:
+    print("Today is Friday")
+elif day == 7:
+    print("Today is Saturday")
+else:
+    print("Not a valid day")
