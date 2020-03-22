f6fb5893e869fd49ffca2ba9efff55d1a43180e6
Feedmemorekurama <61495013+Feedmemorekurama@users.noreply.github.com>
2020-03-22 15:37:25 +0200

    Revert "homework 6 completed"

--- homework_bernadska/hw_6/palindrom.py
+++
@@ -1,6 +0,0 @@
-my_wd = str(input("Enter your word:"))
-a = my_wd[::-1]
-if my_wd == a :
-    print("palindrom")
-else:
-    print("not palindrom")
--- homework_bernadska/hw_6/wolf_and_sheeps.py
+++
@@ -1,9 +0,0 @@
-attack_wolf = ["sheep","sheep","sheep","wolf","sheep"]
-
-attack_wolf.reverse()
-for i in attack_wolf: 
-    if attack_wolf.index("sheep") == 0 and attack_wolf.index("wolf") == 1:
-        print("Oi! Sheep number 1! You are about to be eaten by a wolf!")
-        break
-    else:
-        print("Wolf will eat another sheep")
