6bf72b413a3698b9f338c96429a0e7ba290f8f50
Vladislav Burkatsky <feedmemorekurama@gmail.com>
2020-04-16 23:03:10 +0300

    fixed file

--- homework_bernadska/hw_10/hello_users.py
+++ homework_bernadska/hw_10/hello_users.py
@@ -5,15 +5,9 @@ roles = {
     "developer": 'Carlos'}
 
 name = input("My name is, ")
-for i in roles.values():
-    if name == 'Markus':
-        print("Hello, admin!")
-    elif name == 'Bobby':
-        print("Hello, maintainer!")
-    elif name == 'Garry':
-        print("Hello, manager!")
-    elif name == 'Carlos':
-        print("Hello, developer!")
-    else:
-        print("Hello, Guess")
-    break
+for role, names in roles.items():
+    if name in names:
+        print(f"Hello, {role}")
+        break
+else:
+    print("Hello, Guess")
