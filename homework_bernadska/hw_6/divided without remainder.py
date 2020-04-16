fb24ad7e10a5272dbbf14658b58e63b78b9d1b8f
Vladislav Burkatsky <feedmemorekurama@gmail.com>
2020-04-16 22:34:31 +0300

    Fixed my script

--- homework_bernadska/hw_6/divided without remainder.py
+++ homework_bernadska/hw_6/divided without remainder.py
@@ -1,14 +1,11 @@
-my_list = [52, 9, 13 ,-84, 7, 25 ,-18, 32]
+my_list = [15, 2, 7 ,17, 35, 5 ,-12, 49]
 new_list = []
 idx = 0
 for i in my_list:
     if idx == 0:
-        idx+=1
+        print("you can't divide by zero")    
     elif i % idx ==0:
         new_list.append(i)
-        
-        idx+=1
-    else:
-        idx+=1
+    idx+=1
 
 print(new_list)
--- homework_bernadska/hw_6/the sum of columns.py
+++ homework_bernadska/hw_6/the sum of columns.py
@@ -1,7 +1,13 @@
-l_1 = [[15, 50, 100],
-      [25, 18, 25]]
-result = [sum(i) for i in zip(*l_1)]
-print(result)
+list_1 = [1, 5, 7]
+list_2 = [5, 6, 3]
+sum_list =[]
+for i in range(0,len(list_1)):
+    if len(list_1) != len(list_2):
+        print("Both lists are not equal")
+        break
+    else:    
+        sum_list = list_1[i] + list_2[i]
+    print(sum_list)
 
 
 
